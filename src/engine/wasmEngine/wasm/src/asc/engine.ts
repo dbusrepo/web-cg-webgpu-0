d2c07ccf45ff8571d35288037bd0c4fc78b8174a
dbusrepo <dbusrepo@gmail.com>
2023-06-25 21:56:56 +0200

    Change wasm bck color to black

--- src/engine/wasmEngine/wasm/src/asc/engine.ts
+++ src/engine/wasmEngine/wasm/src/asc/engine.ts
@@ -96,7 +96,7 @@ function render(): void {
 
   // const t0 = <u64>process.hrtime();
   // if (workerIdx == MAIN_THREAD_IDX) {
-  draw.clearBg(s, e, 0xff_ff_00_00); // ABGR
+  draw.clearBg(s, e, 0xff_00_00_00); // ABGR
   // }
 
   // logi(c++);
