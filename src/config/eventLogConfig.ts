2eb311ae4feebd9eb8d259ef3dbec508ab7c957f
dbusrepo <dbusrepo@gmail.com>
2023-07-15 17:01:56 +0200

    Reduce event log height to 0.3

--- src/config/eventLogConfig.ts
+++ src/config/eventLogConfig.ts
@@ -9,7 +9,7 @@ const eventLogConfig = {
   // canvas container (both are part of the panel container of the window), the
   // height of the event log container is given as a % of the canvas container
   // height (aka the canvas display height)
-  percHeightWin: 0.4, // 40% when win in its own container
+  percHeightWin: 0.3, // 30% when win in its own container
   percHeightFull: 0.2, // 15 % of fs height in the same container
   // ************************************************************************
 
